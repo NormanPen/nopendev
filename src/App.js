baa428da6beae3e4c7125fef47cbf6b555935e7c
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2023-03-07 21:09:19 +0100

    add privacy

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './sites/Home';
 import Social from './components/Social';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Imprint from './sites/Imprint';
 
 function App() {
     return (
@@ -13,6 +14,8 @@ function App() {
                 <div className='px-6 bg-nope-flieder-dark dark:bg-nope-grey-dark pt-6 '>
                     <Routes>
                         <Route path='/' element={<Home />} />
+                        <Route path='/impressum' element={<Imprint />} />
+                        <Route path='/imprint' element={<Imprint />} />
                     </Routes>
                     <Social />
                     <Footer />
---
+++ src/sites/Imprint/index.js
@@ -0,0 +1,9 @@
+function Imprint() {
+  return (
+    <div>
+      <h1>Imprint</h1>
+    </div>
+  );
+}
+
+export default Imprint;
